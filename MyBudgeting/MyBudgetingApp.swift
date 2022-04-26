9225e1b57c87b9ace40536ecfb9f9810ba781497
gusde27 <gusdechang24@gmail.com>
2022-04-26 14:39:58 +0800

    Initial Commit

---
+++ MyBudgeting/MyBudgetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetingApp.swift
+//  MyBudgeting
+//
+//  Created by I Gede Bagus Wirawan on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
